22e9c125de519fb95bbb454673d81c43f87181de
Andreas <201608930@post.au.dk>
2019-03-04 10:34:32 +0100

    Add project files.

---
+++ DAB.erBook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DAB.erBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
